ef3ffd2ea6aeaf86f814f90b45bdc9f5fdd13bbd
quocthai07 <dongquocthai.31082006@gmail.com>
2025-12-07 15:33:26 +0700

    Fix

--- Services/Dijkstra.cs
+++ Services/Dijkstra.cs
@@ -1,8 +1,67 @@
-﻿namespace Flight_Dịkstra
+﻿using Flight_Dijkstra;
+using System;
+using System.Collections.Generic;
+
+namespace Flight_Dịkstra
 {
 
     public class Dijkstra
     {
+        private List<double> _dist;
+        private List<int> _prev;
+        private Graph _graph;
 
+        public List<double> Dist
+        {
+            get => _dist;
+            set => _dist = value;
+        }
+
+        public List<int> Prev
+        {
+            get => _prev;
+            set => _prev = value;
+        }
+
+        public Graph Graph
+        {
+            get => _graph;
+            set => _graph = value;
+        }
+
+        public Dijkstra()
+        {
+            _dist = new List<double>();
+            _prev = new List<int>();
+            _graph = new Graph();
+        }
+
+        public List<int> GetShortestPath(int startId, int destId)
+        {
+            if (Math.Abs(_dist[destId] - Double.MaxValue) < 10e-9)
+            {
+                return new List<int>();
+            }
+            var st = new Stack<int>();
+            st.Push(destId);
+
+            while (st.Peek() != startId)
+            {
+                st.Push(_prev[st.Peek()]);
+            }
+
+            var res = new List<int>();
+            while (st.Count != 0)
+            {
+                res.Add(st.Pop());
+            }
+
+            return res;
+        }
+
+        public double GetDistance(int destId)
+        {
+            return _dist[destId];
+        }
     }
 }
